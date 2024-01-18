cf5286616c4b773f884d08901df30bfe948bba94
Onyekachukwu-Nweke <nwekeejioforscheller@gmail.com>
2024-01-18 20:31:27 +0100

    feat: React-redux - Creating user reducer

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import './index.scss';
 import App from './App';
 
 // Contexts
-import { UserProvider } from './context/user.context';
 import { CategoriesProvider } from './context/categories.context';
 import { CartProvider } from './context/cart.context'; 
 import { store } from './store/store';
@@ -19,13 +18,11 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <UserProvider>
-          <CategoriesProvider>
-            <CartProvider>
-              <App />
-            </CartProvider>
-          </CategoriesProvider>
-        </UserProvider>
+        <CategoriesProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </CategoriesProvider>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>

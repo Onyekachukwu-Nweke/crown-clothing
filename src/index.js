cf2f594143a8ca787240aef046b4c398f264e9ac
Onyekachukwu-Nweke <nwekeejioforscheller@gmail.com>
2024-01-18 21:32:21 +0100

    feat: Category selectors redux

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import './index.scss';
 import App from './App';
 
 // Contexts
-import { CategoriesProvider } from './context/categories.context';
 import { CartProvider } from './context/cart.context'; 
 import { store } from './store/store';
 
@@ -18,11 +17,9 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>

90a4ed7e43677c36af3a53b740ef76240d1ff905
Onyekachukwu Nweke <nwekeejioforscheller@gmail.com>
2024-05-31 22:16:58 +0100

    last commit continuation

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,11 +1,11 @@
 import { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { UseDispatch, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
 import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
-import { setCategoriesMap } from '../../store/categories/category.action';
+import { setCategories } from '../../store/categories/category.action';
 
 const Shop = () => {
   const dispatch = useDispatch()
@@ -13,8 +13,8 @@ const Shop = () => {
   useEffect(() => {
     const getCategoriesMap = async () => {
       const categoriesArray = await getCategoriesAndDocuments('categories');
-      console.log(categoriesArray);
-      // dispatch(setCategoriesMap(categoryMap));
+      // console.log(categoriesArray)
+      dispatch(setCategories(categoriesArray));
     };
 
     getCategoriesMap();

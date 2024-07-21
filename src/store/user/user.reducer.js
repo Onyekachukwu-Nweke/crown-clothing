ed34ffecd5669947491703d042440c7bc5015f6f
Onyekachukwu-Nweke <nwekeejioforscheller@gmail.com>
2024-07-21 17:45:16 +0100

    feat: signin sagas

--- src/store/user/user.reducer.js
+++ src/store/user/user.reducer.js
@@ -2,7 +2,9 @@ import { USER_ACTION_TYPES } from "./user.types";
 
 const INITIAL_STATE = {
   currentUser: null,
-}
+  isLoading: false,
+  error: null,
+};
 
 // Using reducers
 export const userReducer = (state = INITIAL_STATE, action) => {
@@ -11,11 +13,13 @@ export const userReducer = (state = INITIAL_STATE, action) => {
   const { type, payload } = action;
 
   switch(type) {
-    case USER_ACTION_TYPES.SET_CURRENT_USER:
+    case USER_ACTION_TYPES.SIGN_IN_SUCCESS:
       return {
         ...state,
         currentUser: payload,
       }
+    case USER_ACTION_TYPES.SIGN_IN_FAILED:
+      return { ...state, error: payload }
     default:
       return state;
   }
